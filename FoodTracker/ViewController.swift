c6cbe4acd71ba021d729c36e76bab3eae11a68e9
Igarashi Nobuo <gonnbe1106@yahoo.co.jp>
2019-10-08 11:53:22 +0900

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by 五十嵐 on 2019/10/08.
+//  Copyright © 2019 ambloom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
